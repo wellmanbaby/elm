cb3cae65501a477690d3ed48e9efc032dae11133
快活的小宝宝 <976905375@qq.com>
2019-04-24 15:57:10 +0800

    完善OrderDetailDao的测试

--- src/test/java/com/elm/demo/dao/OrderDetailDaoTest.java
+++ src/test/java/com/elm/demo/dao/OrderDetailDaoTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -21,19 +22,21 @@ public class OrderDetailDaoTest {
     @Test
     public void saveTest(){
         OrderDetail orderDetail = new OrderDetail();
-        orderDetail.setDetailId("1234567");
-        orderDetail.setOrderId("1111111");
-        orderDetail.setProductId("12121212");
-        orderDetail.setProductIcon("http://xxxxx.jpg");
-        orderDetail.setProductName("熊掌");
-        orderDetail.setProductPrice(new BigDecimal(222));
-        orderDetail.setProductQuantity(2);
+        orderDetail.setDetailId("123456789");
+        orderDetail.setOrderId("2222222");
+        orderDetail.setProductId("13131313");
+        orderDetail.setProductIcon("http://xxxxwwx.jpg");
+        orderDetail.setProductName("鱼");
+        orderDetail.setProductPrice(new BigDecimal(34));
+        orderDetail.setProductQuantity(33);
         OrderDetail result = orderDetailDao.save(orderDetail);
         Assert.assertNotNull(result);
     }
 
     @Test
     public void findByOrderId() throws Exception {
-
+        List<OrderDetail> orderDetailList = orderDetailDao.findByOrderId("1111111");
+        System.out.println(orderDetailList);
+        Assert.assertNotEquals(0,orderDetailList.size());
     }
 }
